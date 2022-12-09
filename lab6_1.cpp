0f09f3695ce75346eac3ca758e0f5ee3ec2fdfe9
fluke.ad <adisorn.sjr@gmail.com>
2022-12-09 16:20:26 +0700

    lab6_2.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,32 @@
 using namespace std;
 
 int main(){
+    int N;
+    int even = 0;
+    int odd = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    
+    if(N != 0){
+        int e = 0;
+        int o = 0;
+        while(N > 0){
+            if(N%2 == 0){
+                e++;
+            }else{
+                o++;
+            }
+        cout << "Enter an integer: ";
+        cin >> N;
+        }
+        cout << "#Even numbers = " << e << endl;
+        cout << "#Odd numbers = " << odd << endl;
+    
+    }else{
+        cout << "#Even numbers = " << even << endl;
+        cout << "Odd numbers = " << odd << endl;
+    }
+
+    
     return 0;
 }
